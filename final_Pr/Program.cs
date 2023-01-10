959fbf1a6a445accd7e06e5b13d417f974000060
Margarita <savmargo98@gmail.com>
2023-01-10 14:02:03 +0800

    Ввод элементов массива с клавиатуры

--- final_Pr/Program.cs
+++ final_Pr/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите 1й элемент массива:");
+string elem1 = Console.ReadLine();
+Console.WriteLine("Введите 2й элемент массива:");
+string elem2 = Console.ReadLine();
+Console.WriteLine("Введите 3й элемент массива:");
+string elem3 = Console.ReadLine();
+Console.WriteLine("Введите 4й элемент массива:");
+string elem4 = Console.ReadLine();
+Console.WriteLine("Введите 5й элемент массива:");
+string elem5 = Console.ReadLine();
+
+
